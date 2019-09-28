e1925c00c0e576f066b57ba20adc4ee6f1506d57
Jared Goodwin <translucency@outlook.com>
2019-09-28 11:19:14 -0700

    Remove installer project.  Add ClickOnce publish settings.

--- Utilities/Publish.ps1
+++ Utilities/Publish.ps1
@@ -3,13 +3,14 @@
    Publishes the Remotely clients.
 .DESCRIPTION
    Publishes the Remotely clients.
-   To deploy the server, supply the following arguments: -rid win10-x64 -outdir path\to\dir
+   To deploy the server, supply the following arguments: -rid win10-x64 -outdir path\to\dir -hostname https://mysite.mydomain.com
 .COPYRIGHT
    Copyright ©  2019 Translucency Software.  All rights reserved.
 .EXAMPLE
    Run it from the Utilities folder (located in the solution directory).
-   Or run "powershell -f PublishClients.ps1 -rid win10-x64 -outdir path\to\dir
+   Or run "powershell -f PublishClients.ps1 -rid win10-x64 -outdir path\to\dir -hostname https://mysite.mydomain.com
 #>
+
 $ErrorActionPreference = "Stop"
 $Year = (Get-Date).Year.ToString()
 $Month = (Get-Date).Month.ToString().PadLeft(2, "0")
@@ -20,10 +21,29 @@ $CurrentVersion = "$Year.$Month.$Day.$Hour$Minute"
 $OutDir = ""
 # RIDs are described here: https://docs.microsoft.com/en-us/dotnet/core/rid-catalog
 $RID = ""
+$Hostname = ""
 $MSBuildPath = (Get-ChildItem -Path "${env:ProgramFiles(x86)}\Microsoft Visual Studio\" -Recurse -Filter "MSBuild.exe" -File)[0].FullName
-$DevEnv = (Get-ChildItem -Path "${env:ProgramFiles(x86)}\Microsoft Visual Studio\" -Recurse -Filter "devenv.com" -File)[0].FullName
-$DisableOutOfProcBuild = (Get-ChildItem -Path "${env:ProgramFiles(x86)}\Microsoft Visual Studio\" -Recurse -Filter "DisableOutOfProcBuild.exe" -File)[0].FullName
 
+
+#region Functions
+
+function Replace-LineInFile($FilePath, $MatchPattern, $ReplaceLineWith, $MaxCount = -1){
+    [string[]]$Content = Get-Content -Path $FilePath
+    $Count = 0
+    for ($i = 0; $i -lt $Content.Length; $i++)
+    {
+        if ($Content[$i] -ne $null -and $Content[$i].Contains($MatchPattern)) {
+            $Content[$i] = $ReplaceLineWith
+            $Count++
+        }
+        if ($MaxCount -gt 0 -and $Count -ge $MaxCount) {
+            break
+        }
+    }
+    ($Content | Out-String).Trim() | Out-File -FilePath $FilePath -Force -Encoding utf8
+}
+
+#endregion
 
 if ([string]::IsNullOrWhiteSpace($MSBuildPath) -or !(Test-Path -Path $MSBuildPath)) {
     Write-Host
@@ -33,21 +53,6 @@ if ([string]::IsNullOrWhiteSpace($MSBuildPath) -or !(Test-Path -Path $MSBuildPat
     return
 }
 
-if ([string]::IsNullOrWhiteSpace($DevEnv) -or !(Test-Path -Path $DevEnv)) {
-    Write-Host
-    Write-Host "ERROR: Unable to find the path to devenv.com." -ForegroundColor Red
-    Write-Host
-    pause
-    return
-}
-
-if ([string]::IsNullOrWhiteSpace($DisableOutOfProcBuild) -or !(Test-Path -Path $DisableOutOfProcBuild)) {
-    Write-Host
-    Write-Host "ERROR: Unable to find the path to DisableOutOfProcBuild.exe." -ForegroundColor Red
-    Write-Host
-    pause
-    return
-}
 
 for ($i = 0; $i -lt $args.Count; $i++)
 { 
@@ -58,6 +63,9 @@ for ($i = 0; $i -lt $args.Count; $i++)
     elseif ($arg.Contains("rid")){
         $RID = $args[$i+1]
     }
+    elseif ($arg.Contains("hostname")){
+        $Hostname = $args[$i+1]
+    }
 }
 
 $Root = (Get-Item -Path $PSScriptRoot).Parent.FullName
@@ -65,18 +73,30 @@ $Root = (Get-Item -Path $PSScriptRoot).Parent.FullName
 Set-Location -Path $Root
 
 # Add Current Version file to root content folder for client update checks.
-Set-Content -Path ".\Server\CurrentVersion.txt" -Value $CurrentVersion.Trim() -Encoding UTF8 -Force
+Set-Content -Path "$Root\Server\CurrentVersion.txt" -Value $CurrentVersion.Trim() -Encoding UTF8 -Force
+
+# Update hostname.
+if ($Hostname.Length -gt 0) {
+    Replace-LineInFile -FilePath "$Root\Desktop.Win\Desktop.Win.csproj" -MatchPattern "<InstallUrl>" -ReplaceLineWith "<InstallUrl>$($Hostname)/Downloads/WinDesktop/</InstallUrl>" -MaxCount 1
+    Replace-LineInFile -FilePath "$Root\Desktop.Win\Services\Config.cs" -MatchPattern "public string Host " -ReplaceLineWith "public string Host { get; set; } = `"$($Hostname)`";" -MaxCount 1
+    Replace-LineInFile -FilePath "$Root\Desktop.Unix\Services\Config.cs" -MatchPattern "public string Host " -ReplaceLineWith "public string Host { get; set; } = `"$($Hostname)`";" -MaxCount 1
+}
+else {
+    Write-Host "ERROR: No hostname parameter was specified.  Application auto-updates will default to the public test server.  Please supply your server's hostname."
+    pause
+    return;
+}
 
     
 # Clear publish folders.
-if ((Test-Path -Path ".\Agent\bin\Release\netcoreapp\win10-x64\publish") -eq $true) {
-	Get-ChildItem -Path ".\Agent\bin\Release\netcoreapp3.0\win10-x64\publish" | Remove-Item -Force -Recurse
+if ((Test-Path -Path "$Root\Agent\bin\Release\netcoreapp\win10-x64\publish") -eq $true) {
+	Get-ChildItem -Path "$Root\Agent\bin\Release\netcoreapp3.0\win10-x64\publish" | Remove-Item -Force -Recurse
 }
-if ((Test-Path -Path  ".\Agent\bin\Release\netcoreapp3.0\win10-x86\publish" ) -eq $true) {
-	Get-ChildItem -Path  ".\Agent\bin\Release\netcoreapp3.0\win10-x86\publish" | Remove-Item -Force -Recurse
+if ((Test-Path -Path  "$Root\Agent\bin\Release\netcoreapp3.0\win10-x86\publish" ) -eq $true) {
+	Get-ChildItem -Path  "$Root\Agent\bin\Release\netcoreapp3.0\win10-x86\publish" | Remove-Item -Force -Recurse
 }
-if ((Test-Path -Path ".\Agent\bin\Release\netcoreapp3.0\linux-x64\publish") -eq $true) {
-	Get-ChildItem -Path ".\Agent\bin\Release\netcoreapp3.0\linux-x64\publish" | Remove-Item -Force -Recurse
+if ((Test-Path -Path "$Root\Agent\bin\Release\netcoreapp3.0\linux-x64\publish") -eq $true) {
+	Get-ChildItem -Path "$Root\Agent\bin\Release\netcoreapp3.0\linux-x64\publish" | Remove-Item -Force -Recurse
 }
 
 
@@ -85,9 +105,9 @@ dotnet publish /p:Version=$CurrentVersion /p:FileVersion=$CurrentVersion --runti
 dotnet publish /p:Version=$CurrentVersion /p:FileVersion=$CurrentVersion --runtime linux-x64 --configuration Release --output "$Root\Agent\bin\Release\netcoreapp3.0\linux-x64\publish" "$Root\Agent"
 dotnet publish /p:Version=$CurrentVersion /p:FileVersion=$CurrentVersion --runtime win10-x86 --configuration Release --output "$Root\Agent\bin\Release\netcoreapp3.0\win10-x86\publish" "$Root\Agent"
 
-New-Item -Path ".\Agent\bin\Release\netcoreapp3.0\win10-x64\publish\ScreenCast\" -ItemType Directory -Force
-New-Item -Path ".\Agent\bin\Release\netcoreapp3.0\win10-x86\publish\ScreenCast\" -ItemType Directory -Force
-New-Item -Path ".\Agent\bin\Release\netcoreapp3.0\linux-x64\publish\ScreenCast\" -ItemType Directory -Force
+New-Item -Path "$Root\Agent\bin\Release\netcoreapp3.0\win10-x64\publish\ScreenCast\" -ItemType Directory -Force
+New-Item -Path "$Root\Agent\bin\Release\netcoreapp3.0\win10-x86\publish\ScreenCast\" -ItemType Directory -Force
+New-Item -Path "$Root\Agent\bin\Release\netcoreapp3.0\linux-x64\publish\ScreenCast\" -ItemType Directory -Force
 
 
 # Publish Linux ScreenCaster
@@ -103,18 +123,25 @@ while ((Test-Path -Path "$PublishDir\Remotely_Desktop.Unix.zip") -eq $false){
 }
 Move-Item -Path "$PublishDir\Remotely_Desktop.Unix.zip" -Destination "$Root\Server\wwwroot\Downloads\Remotely_Desktop.Unix.zip" -Force
 
+
 # Build .NET Framework ScreenCaster (32-bit)
 &"$MSBuildPath" "$Root\ScreenCast.Win" /t:Build /p:Configuration=Release /p:Platform=x86
 
 # Copy 32-bit .NET Framework ScreenCaster to Agent output folder.
-Get-ChildItem -Path ".\ScreenCast.Win\bin\x86\Release\" -Exclude "*.xml" | Copy-Item -Destination ".\Agent\bin\Release\netcoreapp3.0\win10-x86\publish\ScreenCast\" -Force
+Get-ChildItem -Path "$Root\ScreenCast.Win\bin\x86\Release\" -Exclude "*.xml" | Copy-Item -Destination ".\Agent\bin\Release\netcoreapp3.0\win10-x86\publish\ScreenCast\" -Force
 
 # Build .NET Framework ScreenCaster (64-bit)
 &"$MSBuildPath" "$Root\ScreenCast.Win" /t:Build /p:Configuration=Release /p:Platform=x64
 
 # Copy 64-bit .NET Framework ScreenCaster to Agent output folder.
-Get-ChildItem -Path ".\ScreenCast.Win\bin\x64\Release\" -Exclude "*.xml" | Copy-Item -Destination ".\Agent\bin\Release\netcoreapp3.0\win10-x64\publish\ScreenCast\" -Force
+Get-ChildItem -Path "$Root\ScreenCast.Win\bin\x64\Release\" -Exclude "*.xml" | Copy-Item -Destination ".\Agent\bin\Release\netcoreapp3.0\win10-x64\publish\ScreenCast\" -Force
 
+
+# Publish Windows GUI App
+$PublishDir = "$Root\Desktop.Win\publish"
+&"$MSBuildPath" "$Root\Desktop.Win" /t:Publish /p:Configuration=Release /p:Platform=AnyCPU
+# Copy to download folder.
+Get-ChildItem -Path "$PublishDir" | Move-Item -Destination "$Root\Server\wwwroot\Downloads\WinDesktop" -Force
 
 
 # Compress Core clients.
@@ -139,22 +166,6 @@ while ((Test-Path -Path "$PublishDir\Remotely-Linux.zip") -eq $false){
 }
 Move-Item -Path "$PublishDir\Remotely-Linux.zip" -Destination "$Root\Server\wwwroot\Downloads\Remotely-Linux.zip" -Force
 
-# Build desktop app installer and copy to Downloads folder.
-Start-Process -FilePath $DisableOutOfProcBuild -Wait
-$VdprojPath = "$Root\Desktop.Win.Installer\Desktop.Win.Installer.vdproj"
-$Vdproj = Get-Content -Path $VdprojPath -Raw
-
-# 64-bit desktop app installer.
-$Vdproj = $Vdproj.Replace('"TargetPlatform" = "3:0"', '"TargetPlatform" = "3:1"')
-Set-Content -Value $Vdproj -Path $VdprojPath -NoNewline
-&"$DevEnv" "$Root\Desktop.Win.Installer\Desktop.Win.Installer.vdproj" /build "Release|x64"
-Copy-Item -Path ".\Desktop.Win.Installer\Release\Remotely_Desktop_Installer.msi" -Destination ".\Server\wwwroot\Downloads\Remotely_Desktop_Installer_x64.msi" -Force
-
-# 32-bit desktop app installer.
-$Vdproj = $Vdproj.Replace('"TargetPlatform" = "3:1"', '"TargetPlatform" = "3:0"')
-Set-Content -Value $Vdproj -Path $VdprojPath -NoNewline
-&"$DevEnv" "$Root\Desktop.Win.Installer\Desktop.Win.Installer.vdproj" /build "Release|x86"
-Copy-Item -Path ".\Desktop.Win.Installer\Release\Remotely_Desktop_Installer.msi" -Destination ".\Server\wwwroot\Downloads\Remotely_Desktop_Installer_x86.msi" -Force
 
 
 if ($RID.Length -gt 0 -and $OutDir.Length -gt 0) {
