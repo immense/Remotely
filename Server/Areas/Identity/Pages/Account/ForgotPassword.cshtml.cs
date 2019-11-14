85145d75ec0fc98ab312195a51d20887e75a5340
Jared Goodwin <translucency@outlook.com>
2019-11-14 11:35:20 -0800

    Fix password reset link.

--- Server/Areas/Identity/Pages/Account/ForgotPassword.cshtml.cs
+++ Server/Areas/Identity/Pages/Account/ForgotPassword.cshtml.cs
@@ -2,13 +2,15 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Text.Encodings.Web;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
-using Remotely.Shared.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.WebUtilities;
+using Remotely.Shared.Models;
 
 namespace Remotely.Server.Areas.Identity.Pages.Account
 {
@@ -48,10 +50,11 @@ namespace Remotely.Server.Areas.Identity.Pages.Account
                 // For more information on how to enable account confirmation and password reset please 
                 // visit https://go.microsoft.com/fwlink/?LinkID=532713
                 var code = await _userManager.GeneratePasswordResetTokenAsync(user);
+                code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
                 var callbackUrl = Url.Page(
                     "/Account/ResetPassword",
                     pageHandler: null,
-                    values: new { code },
+                    values: new { area = "Identity", code },
                     protocol: Request.Scheme);
 
                 await _emailSender.SendEmailAsync(
