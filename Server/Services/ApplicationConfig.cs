995f1cf22a44abd38be663b6a0095f464a3b42e2
Jared Goodwin <translucency@outlook.com>
2019-12-10 10:28:03 -0800

    Added dark/light theme option.

--- Server/Services/ApplicationConfig.cs
+++ Server/Services/ApplicationConfig.cs
@@ -14,28 +14,27 @@ namespace Remotely.Server.Services
         {
             Config = config;
         }
-        public string DefaultPrompt => Config["ApplicationOptions:DefaultPrompt"];
-        public string DBProvider => Config["ApplicationOptions:DBProvider"];
-        public bool EnableWindowsEventLog => bool.Parse(Config["ApplicationOptions:EnableWindowsEventLog"]);
+        public bool AllowApiLogin => bool.Parse(Config["ApplicationOptions:AllowApiLogin"]);
         public bool AllowSelfRegistration => bool.Parse(Config["ApplicationOptions:AllowSelfRegistration"]);
         public double DataRetentionInDays => double.Parse(Config["ApplicationOptions:DataRetentionInDays"]);
-        public bool RemoteControlRequiresAuthentication => bool.Parse(Config["ApplicationOptions:RemoteControlRequiresAuthentication"]);
-        public double RemoteControlSessionLimit => double.Parse(Config["ApplicationOptions:RemoteControlSessionLimit"]);
+        public string DBProvider => Config["ApplicationOptions:DBProvider"];
+        public string DefaultPrompt => Config["ApplicationOptions:DefaultPrompt"];
+        public bool EnableWindowsEventLog => bool.Parse(Config["ApplicationOptions:EnableWindowsEventLog"]);
+        public string[] KnownProxies => Config.GetSection("ApplicationOptions:KnownProxies").Get<string[]>();
         public bool RecordRemoteControlSessions => bool.Parse(Config["ApplicationOptions:RecordRemoteControlSessions"]);
         public bool RedirectToHTTPS => bool.Parse(Config["ApplicationOptions:RedirectToHTTPS"]);
-        public bool AllowApiLogin => bool.Parse(Config["ApplicationOptions:AllowApiLogin"]);
-        public bool UseHSTS => bool.Parse(Config["ApplicationOptions:RedirectToHTTPS"]);
-        public string[] TrustedCorsOrigins => Config.GetSection("ApplicationOptions:TrustedCorsOrigins").Get<string[]>();
-        public string[] KnownProxies => Config.GetSection("ApplicationOptions:KnownProxies").Get<string[]>();
+        public bool RemoteControlRequiresAuthentication => bool.Parse(Config["ApplicationOptions:RemoteControlRequiresAuthentication"]);
+        public double RemoteControlSessionLimit => double.Parse(Config["ApplicationOptions:RemoteControlSessionLimit"]);
+        public string SmtpDisplayName => Config["ApplicationOptions:SmtpDisplayName"];
+        public string SmtpEmail => Config["ApplicationOptions:SmtpEmail"];
+        public bool SmtpEnableSsl => bool.Parse(Config["ApplicationOptions:SmtpEnableSsl"]);
         public string SmtpHost => Config["ApplicationOptions:SmtpHost"];
+        public string SmtpPassword => Config["ApplicationOptions:SmtpPassword"];
         public int SmtpPort => int.Parse(Config["ApplicationOptions:SmtpPort"]);
         public string SmtpUserName => Config["ApplicationOptions:SmtpUserName"];
-        public string SmtpPassword => Config["ApplicationOptions:SmtpPassword"];
-        public string SmtpEmail => Config["ApplicationOptions:SmtpEmail"];
-        public string SmtpDisplayName => Config["ApplicationOptions:SmtpDisplayName"];
-        public bool SmtpEnableSsl => bool.Parse(Config["ApplicationOptions:SmtpEnableSsl"]);
-
-
+        public string Theme => Config["ApplicationOptions:Theme"];
+        public string[] TrustedCorsOrigins => Config.GetSection("ApplicationOptions:TrustedCorsOrigins").Get<string[]>();
+        public bool UseHSTS => bool.Parse(Config["ApplicationOptions:RedirectToHTTPS"]);
         private IConfiguration Config { get; set; }
     }
 }
