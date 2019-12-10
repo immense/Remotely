881151afe935d4f843f97d1c223ba80121d3430e
Jared Goodwin <translucency@outlook.com>
2019-12-10 10:28:03 -0800

    Added dark/light theme option.

--- Server/wwwroot/scripts/UI.ts
+++ Server/wwwroot/scripts/UI.ts
@@ -122,7 +122,7 @@ export function ShowModal(title: string, modalBodyHtml: string, buttonsHTML: str
             (ev.currentTarget as HTMLElement).parentElement.remove();
         }
     });
-    $("#" + modalID)["modal"]();
+    $("#" + modalID).modal("show");
     return wrapperDiv;
 };
 
