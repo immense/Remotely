21e670ca661d02bf68cac090550c5208211a08d9
Jared Goodwin <translucency@outlook.com>
2019-12-22 22:05:36 -0800

    Convert ScreenCast.Win to .NET Core.

---
+++ ScreenCast.Win_Old/Capture/BitBltCapture.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Forms;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Remotely.ScreenCast.Core.Services;
+using System.Threading;
+using Remotely.ScreenCast.Core.Interfaces;
+
+namespace Remotely.ScreenCast.Win.Capture
+{
+    public class BitBltCapture : ICapturer
+    {
+        public BitBltCapture()
+        {
+            Init();
+        }
+
+        public bool CaptureFullscreen { get; set; } = true;
+        public Bitmap CurrentFrame { get; set; }
+        public Rectangle CurrentScreenBounds { get; set; } = Screen.PrimaryScreen.Bounds;
+        public bool IsCapturing { get; set; }
+        public int PauseForMilliseconds { get; set; }
+        public Bitmap PreviousFrame { get; set; }
+        public event EventHandler<Rectangle> ScreenChanged;
+        public int SelectedScreen { get; private set; } = Screen.AllScreens.ToList().IndexOf(Screen.PrimaryScreen);
+        private Graphics Graphic { get; set; }
+
+        private object ScreenLock { get; } = new object();
+
+        public void Capture()
+        {
+            try
+            {
+                lock (ScreenLock)
+                {
+                    PreviousFrame = (Bitmap)CurrentFrame.Clone();
+                    Graphic.CopyFromScreen(CurrentScreenBounds.Left, CurrentScreenBounds.Top, 0, 0, new Size(CurrentScreenBounds.Width, CurrentScreenBounds.Height));
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.Write(ex);
+                Init();
+            }
+        }
+
+        public void Dispose()
+        {
+            Graphic.Dispose();
+            CurrentFrame.Dispose();
+            PreviousFrame.Dispose();
+        }
+
+        public int GetScreenCount()
+        {
+            return Screen.AllScreens.Length;
+        }
+
+        public Rectangle GetVirtualScreenBounds()
+        {
+            return SystemInformation.VirtualScreen;
+        }
+
+        public void Init()
+        {
+            CurrentFrame = new Bitmap(CurrentScreenBounds.Width, CurrentScreenBounds.Height, PixelFormat.Format32bppArgb);
+            PreviousFrame = new Bitmap(CurrentScreenBounds.Width, CurrentScreenBounds.Height, PixelFormat.Format32bppArgb);
+            Graphic = Graphics.FromImage(CurrentFrame);
+        }
+
+        public void SetSelectedScreen(int screenNumber)
+        {
+            if (screenNumber == SelectedScreen)
+            {
+                return;
+            }
+            lock (ScreenLock)
+            {
+                if (GetScreenCount() >= screenNumber + 1)
+                {
+                    SelectedScreen = screenNumber;
+                }
+                else
+                {
+                    SelectedScreen = 0;
+                }
+                CurrentScreenBounds = Screen.AllScreens[SelectedScreen].Bounds;
+                CaptureFullscreen = true;
+                Init();
+                ScreenChanged?.Invoke(this, CurrentScreenBounds);
+            }
+        }
+    }
+}
---
+++ ScreenCast.Win_Old/Program.cs
@@ -0,0 +1,122 @@
+﻿using Remotely.Shared.Models;
+using Remotely.ScreenCast.Core;
+using Remotely.ScreenCast.Core.Services;
+using System;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Remotely.Shared.Win32;
+using System.Threading;
+using Remotely.ScreenCast.Win.Services;
+using Remotely.ScreenCast.Core.Interfaces;
+using Remotely.ScreenCast.Win.Capture;
+using Remotely.ScreenCast.Core.Sockets;
+
+namespace Remotely.ScreenCast.Win
+{
+    public class Program
+	{
+        public static Conductor Conductor { get; private set; }
+        public static CursorIconWatcher CursorIconWatcher { get; private set; }
+        public static async void CursorIconWatcher_OnChange(object sender, CursorInfo cursor)
+        {
+            if (Conductor?.CasterSocket != null)
+            {
+                await Conductor.CasterSocket.SendCursorChange(cursor, Conductor.Viewers.Keys.ToList());
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            try
+            {
+                AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+                CursorIconWatcher = new CursorIconWatcher(Conductor);
+                var screenCaster = new WinScreenCaster(CursorIconWatcher);
+                var clipboardService = new WinClipboardService();
+                clipboardService.BeginWatching();
+                var casterSocket = new CasterSocket(new WinInput(), screenCaster, new WinAudioCapturer(), clipboardService);
+                Conductor = new Conductor(casterSocket, screenCaster);
+                Conductor.ProcessArgs(args);
+
+                Conductor.Connect().ContinueWith(async (task) =>
+                {
+                    CursorIconWatcher.OnChange += CursorIconWatcher_OnChange;
+                    await Conductor.CasterSocket.SendDeviceInfo(Conductor.ServiceID, Environment.MachineName, Conductor.DeviceID);
+                    CheckInitialDesktop();
+                    await CheckForRelaunch();
+                    Conductor.IdleTimer = new IdleTimer(Conductor.Viewers);
+                    Conductor.IdleTimer.Start();
+
+                    await HandleConnection(Conductor);
+                });
+
+                Thread.Sleep(Timeout.Infinite);
+            }
+            catch (Exception ex)
+            {
+                Logger.Write(ex);
+                throw;
+            }
+        }
+
+        private static async Task CheckForRelaunch()
+        {
+
+            if (Conductor.ArgDict.ContainsKey("relaunch"))
+            {
+                Logger.Write($"Resuming after relaunch in desktop {Conductor.CurrentDesktopName}.");
+                var viewersString = Conductor.ArgDict["viewers"];
+                var viewerIDs = viewersString.Split(",".ToCharArray());
+                await Conductor.CasterSocket.NotifyViewersRelaunchedScreenCasterReady(viewerIDs);
+            }
+            else
+            {
+                await Conductor.CasterSocket.NotifyRequesterUnattendedReady(Conductor.RequesterID);
+            }
+        }
+
+        private static void CheckInitialDesktop()
+        {
+            var desktopName = Win32Interop.GetCurrentDesktop();
+            if (desktopName.ToLower() != Conductor.CurrentDesktopName.ToLower())
+            {
+                Conductor.CurrentDesktopName = desktopName;
+                Logger.Write($"Setting initial desktop to {desktopName}.");
+                Conductor.ArgDict["desktop"] = desktopName;
+                var openProcessString = Assembly.GetExecutingAssembly().Location;
+                foreach (var arg in Conductor.ArgDict)
+                {
+                    openProcessString += $" -{arg.Key} {arg.Value}";
+                }
+                var result = Win32Interop.OpenInteractiveProcess(openProcessString, desktopName, true, out _);
+                if (!result)
+                {
+                    Logger.Write($"Desktop relaunch to {desktopName} failed.");
+                }
+                Environment.Exit(0);
+            }
+        }
+
+        private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            Logger.Write((Exception)e.ExceptionObject);
+        }
+
+
+        private static async Task HandleConnection(Conductor conductor)
+        {
+            while (true)
+            {
+                var desktopName = Win32Interop.GetCurrentDesktop();
+                if (desktopName.ToLower() != conductor.CurrentDesktopName.ToLower() && conductor.Viewers.Count > 0)
+                {
+                    conductor.CurrentDesktopName = desktopName;
+                    Logger.Write($"Switching desktops to {desktopName}.");
+                    Win32Interop.SwitchToInputDesktop();
+                }
+                await Task.Delay(1000);
+            }
+        }
+    }
+}
