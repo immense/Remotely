fe6ba8ee8607e09424d162f779fb3c06b7d761d9
Jared Goodwin <translucency@outlook.com>
2020-02-18 20:10:43 -0800

    Create installer project.

---
+++ Agent.Installer.Win/Services/InstallerService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration.Install;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Remotely.Agent.Installer.Win.Services
+{
+    public class InstallerService
+    {
+        private void InstallService(List<string> args)
+        {
+            try
+            {
+                Logger.Write("Install started.");
+                var installPath = System.Reflection.Assembly.GetExecutingAssembly().Location;
+                var serv = ServiceController.GetServices().FirstOrDefault(ser => ser.ServiceName == "Remotely_Service");
+                if (serv == null)
+                {
+                    string[] command = new String[] { "/assemblypath=" + installPath };
+                    ServiceInstaller ServiceInstallerObj = new ServiceInstaller();
+                    InstallContext Context = new InstallContext("", command);
+                    ServiceInstallerObj.Context = Context;
+                    ServiceInstallerObj.DisplayName = "Remotely Service";
+                    ServiceInstallerObj.Description = "Background service that maintains a connection to the Remotely server.  The service is used for remote support and maintenance by this computer's administrators.";
+                    ServiceInstallerObj.ServiceName = "Remotely_Service";
+                    ServiceInstallerObj.StartType = ServiceStartMode.Automatic;
+                    ServiceInstallerObj.DelayedAutoStart = true;
+                    ServiceInstallerObj.Parent = new ServiceProcessInstaller();
+
+                    System.Collections.Specialized.ListDictionary state = new System.Collections.Specialized.ListDictionary();
+                    ServiceInstallerObj.Install(state);
+                }
+                serv = ServiceController.GetServices().FirstOrDefault(ser => ser.ServiceName == "Remotely_Service");
+                if (serv != null && serv.Status != ServiceControllerStatus.Running)
+                {
+                    serv.Start();
+                }
+                var psi = new ProcessStartInfo("cmd.exe", "/c sc.exe failure \"Remotely_Service\" reset=5 actions=restart/5000");
+                psi.WindowStyle = ProcessWindowStyle.Hidden;
+                Process.Start(psi).WaitForExit();
+            }
+            catch (Exception ex)
+            {
+                Logger.Write(ex);
+            }
+        }
+    }
+}
---
+++ Agent.Installer.Win/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Remotely.Agent.Installer.Win.ViewModels
+{
+    public class MainWindowViewModel
+    {
+        public InstallerSettings ReadInstallerSettings()
+        {
+            try
+            {
+                var fileBytes = File.ReadAllBytes(Assembly.GetExecutingAssembly().Location);
+                using (var readStream = new MemoryStream(fileBytes))
+                using (var br = new BinaryReader(readStream))
+                {
+                    readStream.Seek(-4, SeekOrigin.End);
+                    var payloadSize = br.ReadInt32();
+                    readStream.Seek(-4 - payloadSize, SeekOrigin.End);
+                    var payloadBytes = br.ReadBytes(payloadSize);
+                    using (var writeStream = new MemoryStream(payloadBytes))
+                    {
+                        writeStream.Seek(0, SeekOrigin.Begin);
+                        var serializer = new DataContractJsonSerializer(typeof(InstallerSettings));
+                        var installerSettings = (InstallerSettings)serializer.ReadObject(writeStream);
+                        return installerSettings;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+    }
+}
