4f8a5f6c95325782b0662034fafa975ad66133e0
Jared Goodwin <translucency@outlook.com>
2020-02-22 15:42:52 -0800

    Add installer to ClientDownloadsController and Publish script.

--- Server/API/ClientDownloadsController.cs
+++ Server/API/ClientDownloadsController.cs
@@ -10,6 +10,9 @@ using Microsoft.Extensions.FileProviders;
 using Microsoft.AspNetCore.Hosting;
 using Remotely.Server.Services;
 using Remotely.Server.Auth;
+using Remotely.Shared.Models;
+using System.Text.Json;
+using System.Text;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -19,13 +22,17 @@ namespace Remotely.Server.API
     [ApiController]
     public class ClientDownloadsController : ControllerBase
     {
-        public ClientDownloadsController(IWebHostEnvironment hostEnv, ApplicationConfig appConfig)
+        public ClientDownloadsController(IWebHostEnvironment hostEnv, 
+            ApplicationConfig appConfig,
+            DataService dataService)
         {
             HostEnv = hostEnv;
             AppConfig = appConfig;
+            DataService = dataService;
         }
 
         private ApplicationConfig AppConfig { get; }
+        private DataService DataService { get; }
         private IWebHostEnvironment HostEnv { get; set; }
 
         [ServiceFilter(typeof(ApiAuthorizationFilter))]
@@ -44,6 +51,7 @@ namespace Remotely.Server.API
 
         private async Task<ActionResult> GetInstallFile(string organizationID, string platformID)
         {
+            var organizationName = DataService.GetOrganizationNameById(organizationID);
             var scheme = AppConfig.RedirectToHttps ? "https" : Request.Scheme;
             var fileContents = new List<string>();
             string fileName;
@@ -52,16 +60,21 @@ namespace Remotely.Server.API
             {
                 case "Win10":
                     {
-                        fileName = $"Install-{platformID}.ps1";
+                        fileName = $"Remotely_Installer.exe";
+                        var filePath = Path.Combine(HostEnv.WebRootPath, "Downloads", $"{fileName}");
+                        fileBytes = await System.IO.File.ReadAllBytesAsync(filePath);
 
-                        fileContents.AddRange(await System.IO.File.ReadAllLinesAsync(Path.Combine(HostEnv.WebRootPath, "Downloads", $"{fileName}")));
+                        var installerSettings = new InstallerSettings()
+                        {
+                            OrganizationID = organizationID,
+                            ServerUrl = $"{scheme}://{Request.Host}",
+                            OrganizationName = organizationName
+                        };
 
-                        var hostIndex = fileContents.IndexOf("[string]$HostName = $null");
-                        var orgIndex = fileContents.IndexOf("[string]$Organization = $null");
+                        fileBytes = fileBytes
+                            .Concat(Encoding.UTF8.GetBytes(JsonSerializer.Serialize(installerSettings)))
+                            .ToArray();
 
-                        fileContents[hostIndex] = $"[string]$HostName = \"{scheme}://{Request.Host}\"";
-                        fileContents[orgIndex] = $"[string]$Organization = \"{organizationID}\"";
-                        fileBytes = System.Text.Encoding.UTF8.GetBytes(string.Join(Environment.NewLine, fileContents));
                         break;
                     }
 
