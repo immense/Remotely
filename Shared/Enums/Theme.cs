6cf917e6077103f94edc86215531ef16b8e8bb60
Jared Goodwin <translucency@outlook.com>
2020-02-23 14:22:16 -0800

    Add theme option to UserOptions.

---
+++ Shared/Enums/Theme.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Remotely.Shared.Enums
+{
+    public enum Theme
+    {
+        Light,
+        Dark
+    }
+}
