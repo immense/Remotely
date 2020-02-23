8ac490e759b06cefa1f3a428e06de78edf33a665
Jared Goodwin <translucency@outlook.com>
2020-02-23 14:22:16 -0800

    Add theme option to UserOptions.

--- Shared/Models/RemotelyUserOptions.cs
+++ Shared/Models/RemotelyUserOptions.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Remotely.Shared.Enums;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
@@ -25,5 +26,8 @@ namespace Remotely.Shared.Models
         [Display(Name = "Bash Shortcut")]
         [StringLength(10)]
         public string CommandModeShortcutBash { get; set; } = "/bash";
+
+        [Display(Name = "Theme")]
+        public Theme Theme { get; set; }
     }
 }
