585ce96ab40fb25715e90899ced4ba5eeb88e711
Jared Goodwin <translucency@outlook.com>
2020-03-07 11:59:24 -0800

    Updater downloads static file.

--- Server/wwwroot/scripts/Pages/IndexNotLoggedIn.js
+++ Server/wwwroot/scripts/Pages/IndexNotLoggedIn.js
@@ -18,9 +18,14 @@ function typeText(textParts, header) {
     else {
         textParts.shift();
         if (textParts.length > 0) {
+            var timeout = 100;
+            if (textParts[0].split('').every(x => x == " ")) {
+                timeout = 800;
+            }
+            console.log(timeout);
             window.setTimeout(() => {
                 typeText(textParts, header);
-            }, 800);
+            }, timeout);
         }
     }
 }
