5a42ff3f7fe718b6618d34ef611179bdc6c6befd
Jared Goodwin <translucency@outlook.com>
2020-03-08 13:37:45 -0700

    Let ServiceProvider be overridden in setter.

--- ScreenCast.Core/ServiceContainer.cs
+++ ScreenCast.Core/ServiceContainer.cs
@@ -6,22 +6,6 @@ namespace Remotely.ScreenCast.Core
 {
     public class ServiceContainer
     {
-        private static IServiceProvider instance;
-
-        public static IServiceProvider Instance
-        {
-            get
-            {
-                return instance;
-            }
-            set
-            {
-                if (instance != null)
-                {
-                    throw new Exception("ServiceProvider can only be set once.");
-                }
-                instance = value;
-            }
-        }
+        public static IServiceProvider Instance { get; set; }
     }
 }
