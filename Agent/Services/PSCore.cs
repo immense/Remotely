9ca644c493ee516ede3c0a0375b766b83a159094
Jared Goodwin <translucency@outlook.com>
2020-03-13 07:35:10 -0700

    Add using around temp PS instance.

--- Agent/Services/PSCore.cs
+++ Agent/Services/PSCore.cs
@@ -63,31 +63,33 @@ namespace Remotely.Agent.Services
             PS.AddScript(input);
             var results = PS.Invoke();
 
-            var ps = PowerShell.Create();
-            ps.AddScript("$args[0] | Out-String");
-            ps.AddArgument(results);
-            var hostOutput = (ps.Invoke()[0].BaseObject as string);
-
-            var verboseOut = PS.Streams.Verbose.ReadAll().Select(x => x.Message).ToList();
-            var debugOut = PS.Streams.Debug.ReadAll().Select(x => x.Message).ToList();
-            var errorOut = PS.Streams.Error.ReadAll().Select(x => x.Exception.ToString() + Environment.NewLine + x.ScriptStackTrace).ToList();
-            var infoOut = PS.Streams.Information.Select(x => x.MessageData.ToString()).ToList();
-            var warningOut = PS.Streams.Warning.Select(x => x.Message).ToList();
-
-            PS.Streams.ClearStreams();
-            PS.Commands.Clear();
-
-            return new PSCoreCommandResult()
+            using (var ps = PowerShell.Create())
             {
-                CommandResultID = commandID,
-                DeviceID = ConfigService.GetConnectionInfo().DeviceID,
-                DebugOutput = debugOut,
-                ErrorOutput = errorOut,
-                VerboseOutput = verboseOut,
-                HostOutput = hostOutput,
-                InformationOutput = infoOut,
-                WarningOutput = warningOut
-            };
+                ps.AddScript("$args[0] | Out-String");
+                ps.AddArgument(results);
+                var hostOutput = (ps.Invoke()[0].BaseObject as string);
+
+                var verboseOut = PS.Streams.Verbose.ReadAll().Select(x => x.Message).ToList();
+                var debugOut = PS.Streams.Debug.ReadAll().Select(x => x.Message).ToList();
+                var errorOut = PS.Streams.Error.ReadAll().Select(x => x.Exception.ToString() + Environment.NewLine + x.ScriptStackTrace).ToList();
+                var infoOut = PS.Streams.Information.Select(x => x.MessageData.ToString()).ToList();
+                var warningOut = PS.Streams.Warning.Select(x => x.Message).ToList();
+
+                PS.Streams.ClearStreams();
+                PS.Commands.Clear();
+
+                return new PSCoreCommandResult()
+                {
+                    CommandResultID = commandID,
+                    DeviceID = ConfigService.GetConnectionInfo().DeviceID,
+                    DebugOutput = debugOut,
+                    ErrorOutput = errorOut,
+                    VerboseOutput = verboseOut,
+                    HostOutput = hostOutput,
+                    InformationOutput = infoOut,
+                    WarningOutput = warningOut
+                };
+            }
         }
 
         private void ProcessIdleTimeout_Elapsed(object sender, ElapsedEventArgs e)
