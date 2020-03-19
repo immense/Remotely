cbbeed91290efb67baad8042d13a6d15651f975a
Jared Goodwin <translucency@outlook.com>
2020-03-19 07:59:49 -0700

    Scaffold alert models and controller.

---
+++ Shared/Models/Alert.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Remotely.Shared.Models
+{
+    public class Alert
+    {
+        [Key]
+        public string ID { get; set; } = Guid.NewGuid().ToString();
+        public DateTimeOffset CreatedOn { get; set; } = DateTimeOffset.Now;
+        public Device Device { get; set; }
+        public string DeviceID { get; set; }
+        public string Message { get; set; }
+        public ICollection<RemotelyUser> SeenBy { get; set; }
+    }
+}
