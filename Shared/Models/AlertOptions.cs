08e672aff447b760a8d619ea167e0fc51b925083
Jared Goodwin <translucency@outlook.com>
2020-03-19 07:59:49 -0700

    Scaffold alert models and controller.

---
+++ Shared/Models/AlertOptions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace Remotely.Shared.Models
+{
+    public class AlertOptions
+    {
+        public string AlertDeviceID { get; set; }
+        public string AlertMessage { get; set; }
+        public string ApiRequestBody { get; set; }
+        public Dictionary<string, string> ApiRequestHeaders { get; set; }
+        public string ApiRequestMethod { get; set; }
+        public string ApiRequestUrl { get; set; }
+        public string EmailBody { get; set; }
+        public string EmailSubject { get; set; }
+        public string EmailTo { get; set; }
+        public bool ShouldAlert { get; set; }
+        public bool ShouldEmail { get; set; }
+        public bool ShouldSendApiRequest { get; set; }
+    }
+}
