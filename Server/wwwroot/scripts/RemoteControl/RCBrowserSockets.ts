9e371dc4a68148eb356de26f39d5f5d7a48c6f9f
Jared Goodwin <translucency@outlook.com>
2020-04-22 12:19:29 -0700

    Add connectionStarted property.

--- Server/wwwroot/scripts/RemoteControl/RCBrowserSockets.ts
+++ Server/wwwroot/scripts/RemoteControl/RCBrowserSockets.ts
@@ -9,6 +9,7 @@ var signalR = window["signalR"];
 
 type HubConnection = {
     start: () => Promise<any>;
+    connectionStarted: boolean;
     closedCallbacks: any[];
     invoke: (...rest) => any;
     stop: () => any;
