e08fe3db26eaa51033199133787875373b816b28
Jared Goodwin <translucency@outlook.com>
2020-04-22 08:57:28 -0700

    Add status messages.

--- Desktop.Win.Wrapper/Install.ps1
+++ Desktop.Win.Wrapper/Install.ps1
@@ -1,1 +1,4 @@
+[System.IO.Directory]::CreateDirectory("$env:AppData\Remotely")
 Invoke-WebRequest -Uri "https://dot.net/v1/dotnet-install.ps1" -OutFile "$env:AppData\Remotely\dotnet-install.ps1"
+&"$env:AppData\Remotely\dotnet-install.ps1" -Runtime dotnet
+&"$env:AppData\Remotely\dotnet-install.ps1" -Runtime windowsdesktop
