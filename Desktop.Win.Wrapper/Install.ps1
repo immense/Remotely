ea9c008f2e151dfcdf8c6e413c8e52a177d0c703
Jared Goodwin <translucency@outlook.com>
2020-04-23 10:36:19 -0700

    Set security protocol.

--- Desktop.Win.Wrapper/Install.ps1
+++ Desktop.Win.Wrapper/Install.ps1
@@ -1,3 +1,4 @@
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12 -bor [System.Net.SecurityProtocolType]::Tls13
 [System.IO.Directory]::CreateDirectory("$env:AppData\Remotely")
 Invoke-WebRequest -Uri "https://dot.net/v1/dotnet-install.ps1" -OutFile "$env:AppData\Remotely\dotnet-install.ps1"
 &"$env:AppData\Remotely\dotnet-install.ps1" -Runtime dotnet
