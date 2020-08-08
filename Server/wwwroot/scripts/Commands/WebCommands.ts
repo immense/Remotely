29df1df67b2f979816be2c4afc552d4d16e86e14
Jared Goodwin <translucency@outlook.com>
2020-08-08 15:54:51 -0700

    Fix file upload progress.

--- Server/wwwroot/scripts/Commands/WebCommands.ts
+++ Server/wwwroot/scripts/Commands/WebCommands.ts
@@ -537,9 +537,16 @@ function uploadFiles(fileList: FileList): Promise<string[]> {
             AddConsoleOutput("File upload failed.");
             reject();
         });
-        xhr.addEventListener("progress", function (e) {
-            AddConsoleOutput("File upload progress: " + String(isFinite(e.loaded / e.total) ? e.loaded / e.total : 0) + "%");
-        });
+
+        xhr.upload.onprogress = (e) => {
+            var currentPercent = isFinite(e.loaded / e.total) ? Math.round(e.loaded / e.total * 100) : 0;
+
+            if (currentPercent != uploadPercent) {
+                var uploadPercent = currentPercent;
+                AddConsoleOutput("File upload progress: " + String(currentPercent) + "%");
+            }
+        };
+
         xhr.send(fd);
     })
    
