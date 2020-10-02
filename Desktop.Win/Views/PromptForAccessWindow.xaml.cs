83ef8c00a55dbc2b32083879c3e17f9b9a52b4e7
Jared <translucency@outlook.com>
2020-10-02 13:37:18 -0700

    Create windows and viewmodels to prompt for remote control access.

---
+++ Desktop.Win/Views/PromptForAccessWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Remotely.Desktop.Win.Views
+{
+    /// <summary>
+    /// Interaction logic for PromptForAccessWindow.xaml
+    /// </summary>
+    public partial class PromptForAccessWindow : Window
+    {
+        public PromptForAccessWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void Window_ContentRendered(object sender, EventArgs e)
+        {
+            Topmost = false;
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+    }
+}
