292fc74519310f7b50bd95fd79e25065687393d2
Jared <translucency@outlook.com>
2020-11-02 08:04:35 -0800

    Revert some changes to Nginx config.

--- Utilities/Ubuntu_Server_Install.sh
+++ Utilities/Ubuntu_Server_Install.sh
@@ -64,11 +64,6 @@ systemctl start nginx
 # Configure Nginx
 nginxConfig="
 
-map \$http_upgrade \$connection_upgrade {
-    default Upgrade;
-    ''      close;
-}
-
 server {
     listen        80;
     server_name   $HostName *.$HostName;
@@ -76,11 +71,52 @@ server {
         proxy_pass         http://localhost:5000;
         proxy_http_version 1.1;
         proxy_set_header   Upgrade \$http_upgrade;
-        proxy_set_header   Connection \$connection_upgrade;
+        proxy_set_header   Connection keep-alive;
         proxy_set_header   Host \$host;
         proxy_cache_bypass \$http_upgrade;
         proxy_set_header   X-Forwarded-For \$proxy_add_x_forwarded_for;
         proxy_set_header   X-Forwarded-Proto \$scheme;
+    }
+
+    location /BrowserHub {	
+        proxy_pass http://localhost:5000;	
+        proxy_http_version 1.1;	
+        proxy_set_header Upgrade \$http_upgrade;	
+        proxy_set_header Connection \"upgrade\";	
+        proxy_set_header Host \$host;	
+        proxy_cache_bypass \$http_upgrade;	
+        proxy_set_header   X-Forwarded-For \$proxy_add_x_forwarded_for;	
+        proxy_set_header   X-Forwarded-Proto \$scheme;	
+    }	
+    location /AgentHub {	
+        proxy_pass http://localhost:5000;	
+        proxy_http_version 1.1;	
+        proxy_set_header Upgrade \$http_upgrade;	
+        proxy_set_header Connection \"upgrade\";	
+        proxy_set_header Host \$host;	
+        proxy_cache_bypass \$http_upgrade;	
+        proxy_set_header   X-Forwarded-For \$proxy_add_x_forwarded_for;	
+        proxy_set_header   X-Forwarded-Proto \$scheme;	
+    }	
+    location /ViewerHub {	
+        proxy_pass http://localhost:5000;	
+        proxy_http_version 1.1;	
+        proxy_set_header Upgrade \$http_upgrade;	
+        proxy_set_header Connection \"upgrade\";	
+        proxy_set_header Host \$host;	
+        proxy_cache_bypass \$http_upgrade;	
+        proxy_set_header   X-Forwarded-For \$proxy_add_x_forwarded_for;	
+        proxy_set_header   X-Forwarded-Proto \$scheme;	
+    }	
+    location /CasterHub {	
+        proxy_pass http://localhost:5000;	
+        proxy_http_version 1.1;	
+        proxy_set_header Upgrade \$http_upgrade;	
+        proxy_set_header Connection \"upgrade\";	
+        proxy_set_header Host \$host;	
+        proxy_cache_bypass \$http_upgrade;	
+        proxy_set_header   X-Forwarded-For \$proxy_add_x_forwarded_for;	
+        proxy_set_header   X-Forwarded-Proto \$scheme;	
     }
 }"
 
