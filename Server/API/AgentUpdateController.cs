9858432821f958dd8ca090f053ae83daa049f536
Jared <translucency@outlook.com>
2021-01-04 14:29:01 -0800

    Check banned devices during agent update.

--- Server/API/AgentUpdateController.cs
+++ Server/API/AgentUpdateController.cs
@@ -1,13 +1,17 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Primitives;
+using Remotely.Server.Hubs;
 using Remotely.Server.Services;
 using Remotely.Shared.Enums;
 using System;
 using System.IO;
+using System.Linq;
 using System.Net;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -23,15 +27,18 @@ namespace Remotely.Server.API
 
         public AgentUpdateController(IWebHostEnvironment hostingEnv,
             IDataService dataService,
-            IApplicationConfig appConfig)
+            IApplicationConfig appConfig,
+            IHubContext<AgentHub> agentHubContext)
         {
             HostEnv = hostingEnv;
             DataService = dataService;
             AppConfig = appConfig;
+            AgentHubContext = agentHubContext;
         }
 
         private IDataService DataService { get; }
-        public IApplicationConfig AppConfig { get; }
+        private IApplicationConfig AppConfig { get; }
+        private IHubContext<AgentHub> AgentHubContext { get; }
         private IWebHostEnvironment HostEnv { get; }
 
 
@@ -48,6 +55,13 @@ namespace Remotely.Server.API
         {
             try
             {
+                var remoteIp = Request?.HttpContext?.Connection?.RemoteIpAddress.ToString();
+
+                if (await CheckForDeviceBan(remoteIp))
+                {
+                    return GetImmediateUninstaller(platform);
+                }
+
                 var startWait = DateTimeOffset.Now;
 
                 while (_downloadingAgents.Count >= AppConfig.MaxConcurrentUpdates)
@@ -73,7 +87,7 @@ namespace Remotely.Server.API
                 var waitTime = DateTimeOffset.Now - startWait;
                 DataService.WriteEvent($"Download started after wait time of {waitTime}.  " + 
                     $"ID: {downloadId}. " +
-                    $"IP: {Request?.HttpContext?.Connection?.RemoteIpAddress}. " +
+                    $"IP: {remoteIp}. " +
                     $"Current Downloads: {_downloadingAgents.Count}.  Max Allowed: {AppConfig.MaxConcurrentUpdates}", EventType.Debug, null);
 
 
@@ -93,7 +107,7 @@ namespace Remotely.Server.API
                     default:
                         DataService.WriteEvent($"Unknown platform requested in { nameof(AgentUpdateController)}. " +
                             $"Platform: {platform}. " +
-                            $"IP: {Request?.HttpContext?.Connection?.RemoteIpAddress}.",
+                            $"IP: {remoteIp}.",
                             EventType.Warning,
                             null);
                         return BadRequest();
@@ -109,6 +123,55 @@ namespace Remotely.Server.API
                 DataService.WriteEvent(ex, null);
                 return StatusCode((int)HttpStatusCode.InternalServerError);
             }
+        }
+
+        private ActionResult GetImmediateUninstaller(string platform)
+        {
+            switch (platform.ToLower())
+            {
+                case "win-x64":
+                case "win-x86":
+                    {
+                        var fileContents = "sc delete Remotely_Service & taskkill /im Remotely_Agent.exe /f";
+                        return File(Encoding.UTF8.GetBytes(fileContents), "application/octet-stream");
+                    }
+                case "linux":
+                    {
+                        var fileContents = "systemctl stop remotely-agent; rm -f /etc/systemd/system/remotely-agent.service;";
+                        return File(Encoding.UTF8.GetBytes(fileContents), "application/octet-stream");
+                    }
+                    break;
+                default:
+                    DataService.WriteEvent($"Unknown platform requested in {nameof(AgentUpdateController)}. " +
+                        $"Platform: {platform}. " +
+                        $"IP: {Request?.HttpContext?.Connection?.RemoteIpAddress}.",
+                        EventType.Warning,
+                        null);
+                    return BadRequest();
+            }
+        }
+
+        private async Task<bool> CheckForDeviceBan(string deviceIp)
+        {
+            if (string.IsNullOrWhiteSpace(deviceIp))
+            {
+                return false;
+            }
+
+            if (AppConfig.BannedDevices.Contains(deviceIp))
+            {
+                DataService.WriteEvent($"Device IP ({deviceIp}) is banned.  Sending uninstall command.", null);
+
+                var bannedDevices = AgentHub.ServiceConnections.Where(x => x.Value.PublicIP == deviceIp);
+                foreach (var bannedDevice in bannedDevices)
+                {
+                    await AgentHubContext.Clients.Client(bannedDevice.Key).SendAsync("UninstallAgent");    
+                }
+
+                return true;
+            }
+
+            return false;
         }
     }
 }
