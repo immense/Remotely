32220ada177b8203998c32738d4458b196799ad9
Jared <translucency@outlook.com>
2021-04-09 19:29:37 -0700

    Remove unused TS/JS files.

--- Server/wwwroot/src/RemoteControl/Models/CommandLineParameter.ts
+++
@@ -1,8 +0,0 @@
-﻿export class CommandLineParameter {
-    constructor(name: string, value: string) {
-        this.Name = name;
-        this.Value = value;
-    }
-    Name: string;
-    Value: string;
-}
