5d9cd0632dda2892505c02673c17125cc050dc2c
dkattan <1424395+dkattan@users.noreply.github.com>
2021-12-03 14:13:25 -0600

    Added get route for sessions

---
+++ Server/API/SessionController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Remotely.Server.Auth;
+using Remotely.Server.Hubs;
+using Remotely.Server.Models;
+
+namespace Remotely.Server.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [ServiceFilter(typeof(ApiAuthorizationFilter))]
+    public class SessionController : ControllerBase
+    {
+
+        // GET: api/<SessionController>
+        [HttpGet]
+        public IEnumerable<RCSessionInfo> Get()
+        {
+            return CasterHub.SessionInfoList.Select(a => a.Value).AsEnumerable();
+        }
+    }
+}
