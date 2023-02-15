0e62543b19ba62463ac4d9f1608fdc50ec33c609
Jared Goodwin <translucency@outlook.com>
2023-02-14 20:01:04 -0800

    Enable account lockout.

--- Server/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Server/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -88,7 +88,7 @@ namespace Remotely.Server.Areas.Identity.Pages.Account
             {
                 // This doesn't count login failures towards account lockout
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
-                var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: false);
+                var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: true);
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");
